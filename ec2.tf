228ddd428d38936dde19d547ba57ba02568aadf4
shahabsb94 <shahabsb94@gmail.com>
2025-12-24 11:45:06 +0530

    Docker+Kubectl+Eksctl

---
+++ ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "Shabu-Terraform-Instance" {
+  ami                    = data.aws_ami.joindevops.id
+  vpc_security_group_ids = [aws_security_group.Shabu-Security.id]
+  instance_type          = "t3.micro"
+
+  root_block_device {
+    volume_size = 30 # in GB <<----- I increased this!
+    volume_type = "gp3"
+  }
+  user_data = file("docker.sh")
+
+  tags = {
+    Name = "docker"
+  }
+}
+
+resource "aws_security_group" "Shabu-Security" {
+  name        = "Shabu-Security"
+  description = "allow-all with out any restriciton"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Semantically equivalent to "all"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all inbound traffic from anywhere (IPv4)"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "allow-all"
+  }
+}
